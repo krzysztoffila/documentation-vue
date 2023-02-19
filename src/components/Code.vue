e441a3a6c5437fc3d727c7b18a9cfcb5dea8d050
krzysztoffila <umowy.fila@gmail.com>
2023-02-19 18:57:48 +0100

    Init Project

---
+++ src/components/Code.vue
@@ -0,0 +1,37 @@
+<template>
+  <pre>
+    <div class="code__clipboard-container">
+        <div>Copied!</div>
+        <svg></svg>
+    </div>
+    <code>{{coloredCode}}</code>
+</pre>
+</template>
+
+<script>
+export default {
+  props: {
+    code: String,
+    extension: String,
+  },
+  data() {
+    return {
+      copied: false,
+    };
+  },
+  computed: {
+    coloredCode() {
+      return this.code;
+    },
+  },
+  methods: {
+    copyToClipboard(content) {},
+    setCopied(copied) {
+      this.copied = copied;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/UI.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <!-- <page-title />
+    <section-title />
+    <decription />
+    <button-component />
+    <info /> -->
+    <code-component extension="vue" code="<h1>test</h1>" />
+    <!-- <warning />
+    <danger />
+    <pagination />
+    <related /> -->
+  </section>
+</template>
+
+<script>
+import CodeComponent from "@/components/Code.vue";
+export default {
+  components: {
+    CodeComponent,
+  },
+};
+</script>
+
+<style>
+</style>

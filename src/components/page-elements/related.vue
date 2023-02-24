6734ab12dc30f05ab9673b8c7876b0999172ab6c
krzysztoffila <umowy.fila@gmail.com>
2023-02-24 20:12:24 +0100

    Add related component

--- src/components/page-elements/related.vue
+++ src/components/page-elements/related.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="related">
-    <!-- <h4>Related</h4> -->
-    <!-- <ul> -->
-    <!-- <li></li> -->
-    <!-- </ul> -->
+    <h3>Related</h3>
+    <ul class="related__list">
+      <li class="related__link"><a href="#">Related 1</a></li>
+      <li><a href="#">Related 2</a></li>
+    </ul>
   </div>
 </template>
 

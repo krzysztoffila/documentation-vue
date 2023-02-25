d8a335e7c7ddfe9cfdcc1ad50d04f1fe5aaee93a
krzysztoffila <umowy.fila@gmail.com>
2023-02-25 12:37:06 +0100

    Add class in pagination component

--- src/components/page-elements/pagination.vue
+++ src/components/page-elements/pagination.vue
@@ -2,7 +2,7 @@
   <div class="pagination p-3">
     <b-button
       :class="[('prev', prev && `${prev}`)]"
-      class="pagination__left"
+      class="pagination__left-button"
       variant="outline-primary"
     >
       <b-icon class="pagination__left__icon" icon="chevron-left"></b-icon>
@@ -10,7 +10,7 @@
     </b-button>
     <b-button
       :class="[('next', next && `${next}`)]"
-      class="pagination__right"
+      class="pagination__right-button"
       variant="outline-primary"
     >
       <b-icon class="pagination__right__icon" icon="chevron-right"></b-icon>

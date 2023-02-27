dea16585d462435407dfe846306aca20c4b0e02d
krzysztoffila <umowy.fila@gmail.com>
2023-02-27 15:24:28 +0100

    Poprano ikony, zmieniono strukturę b-icon, binodwanie ikon z obiektu icons

--- src/components/page-elements/info.vue
+++ src/components/page-elements/info.vue
@@ -1,29 +1,30 @@
 <template>
-  <!-- w zaleznosci od typu zmiana koloru i ikonki  uzyc if https://bootstrap-vue.org/docs/components/card#background-and-border-variants-->
   <div :class="['info', type && `info--${type}`]" class="info__container">
-    <div class="info__container-icon">
-      <!-- ikony -->
-      <b-icon
-        v-if="info"
-        icon="info-circle"
-        :variant="type"
-        class="mx-2"
-      ></b-icon>
-      <b-icon
-        v-else-if="type == `warning`"
-        icon="exclamation-circle"
-        :variant="type"
-        class="mx-2"
-      ></b-icon>
-      <b-icon
-        v-else-if="type == `danger`"
-        icon="exclamation-triangle"
-        :variant="type"
-        class="mx-2"
-      ></b-icon>
+    <div class="info__container-icon mx-2">
+      <svg
+        viewBox="0 0 16 16"
+        width="1em"
+        height="1em"
+        focusable="false"
+        role="img"
+        aria-label="exclamation circle"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        class="bi-exclamation-circle b-icon bi info"
+      >
+        <g>
+          <path
+            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+          ></path>
+          <path
+            d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"
+          ></path>
+        </g>
+      </svg>
+      <b-icon :class="info" :icon="icons[type]" :variant="type"></b-icon>
     </div>
     <div class="info__container-text" :variant="type">
-      <b> | INFO |{{ type }} </b><br />
+      <b>{{ type }} </b><br />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
         inventore error dolorum corporis. Quisquam dolorem consectetur fuga
@@ -54,8 +55,9 @@ export default {
   data() {
     return {
       icons: {
-        warning: "",
-        danger: "",
+        info: "lightbulb",
+        warning: "exclamation-circle",
+        danger: "exclamation-triangle",
       },
     };
   },

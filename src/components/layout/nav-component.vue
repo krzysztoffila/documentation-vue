95a5703e4124832fb9093a0ef5ed7a58c74033ad
krzysztoffila <umowy.fila@gmail.com>
2023-04-18 14:58:34 +0200

    DV-1 - Dodano logo do navbaru

--- src/components/layout/nav-component.vue
+++ src/components/layout/nav-component.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <b-navbar type="dark" variant="dark">
+      <img src="@/assets/logo.svg" alt="logo" height="40px" />
       <b-navbar-nav>
         <b-nav-item href="#">Vue.js</b-nav-item>
       </b-navbar-nav>

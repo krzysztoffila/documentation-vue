356b7f87dbd65e451048111c01a7af2afdf64426
krzysztoffila <umowy.fila@gmail.com>
2023-06-13 08:01:45 +0200

    DO SPRAWDZENIA

--- src/components/layout/left-menu.vue
+++ src/components/layout/left-menu.vue
@@ -1,17 +1,35 @@
 <template>
-  <aside class="left-menu p-3">
+  <aside class="topnav left-menu p-3" id="myTopnav">
     <ul class="left-menu__list">
-      <li class="left-menu__list__link"><a href="#">Artykuł 1</a></li>
-      <li class="left-menu__list__link"><a href="#">Artykuł 2</a></li>
-      <li class="left-menu__list__link"><a href="#">Artykuł 3</a></li>
-      <li class="left-menu__list__link"><a href="#">Artykuł 4</a></li>
-      <li class="left-menu__list__link"><a href="#">Artykuł 5</a></li>
+      <li class="left-menu__list__link"><a href="#">Article 1</a></li>
+      <li class="left-menu__list__link"><a href="#">Article 2</a></li>
+      <li class="left-menu__list__link"><a href="#">Article 3</a></li>
+      <li class="left-menu__list__link"><a href="#">Article 4</a></li>
+      <li class="left-menu__list__link"><a href="#">Article 5</a></li>
+    </ul>
+    <ul class="left-menu__toolbar">
+      <li>
+        <a href="javascript:void(0);" class="icon" @click="showList">
+          <b-icon icon="menu-up" aria-hidden="true"></b-icon>
+        </a>
+      </li>
     </ul>
   </aside>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    showList() {
+      let x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -33,6 +51,21 @@ export default {};
   color: #b3bcc3;
   &:hover {
     color: #fff;
+  }
+}
+.left-menu__toolbar {
+  list-style-type: none;
+}
+.icon {
+  display: none;
+}
+@media screen and (max-width: 992px) {
+  .left-menu__list {
+    display: none;
+  }
+  .left-menu__toolbar .icon {
+    float: right;
+    display: block;
   }
 }
 </style>

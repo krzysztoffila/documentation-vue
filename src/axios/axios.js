3a35149f9f3ab17024e26113033bea7fa697cc20
krzysztoffila <umowy.fila@gmail.com>
2023-08-13 08:18:38 +0200

    [DV-24] - Błąd CORS w konfiguracji Axios

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -5,6 +5,8 @@ export const axiosApi = axios.create({
     loginURL: `https://documentation-vue.projects.codennection.pl/api/auth/login`,
     registerURL: `https://documentation-vue.projects.codennection.pl/api/auth/register`,
     headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-type': 'application/json',
         Authorization: 'Bearer {token}'
     }
 })

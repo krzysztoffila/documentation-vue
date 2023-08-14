66674f07f772896dfbd5b2c48520f15d1462643f
dlewicki <dlewicki@media4u.pl>
2023-08-14 19:55:43 +0200

    [DV-24] konfiguracja axiosa - nagłówek Authorization

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
-export const axiosApi = axios.create({
-    baseURL: `https://documentation-vue.projects.codennection.pl`,
-    loginURL: `https://documentation-vue.projects.codennection.pl/api/auth/login`,
-    registerURL: `https://documentation-vue.projects.codennection.pl/api/auth/register`,
+const axiosOptions = {
+    baseURL: `https://documentation-vue.projects.codennection.pl/api`,
     headers: {
         'Access-Control-Allow-Origin': '*',
-        'Content-type': 'application/json',
-        Authorization: 'Bearer {token}'
+        'Content-type': 'application/json'
     }
-})
+}
+
+const tokenJWT = localStorage.getItem('token')
+
+if(tokenJWT) {
+    axiosOptions.headers.Authorization = `Bearer ${tokenJWT}`
+}
+
+export const axiosApi = axios.create(axiosOptions)

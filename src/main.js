e9e1a22922dd48e9bb01a7e9f4bf3a41b8b80701
krzysztoffila <umowy.fila@gmail.com>
2023-08-21 12:51:58 +0200

    [DV-21] - Zainstalowano poprawnie vClickOutside

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import {
   BootstrapVue,
   IconsPlugin
 } from 'bootstrap-vue';
-
+import vClickOutside from "v-click-outside";
 
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(vClickOutside);
 
 new Vue({
   router,
